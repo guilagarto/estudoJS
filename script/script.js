edc88418852196df48acb7f97309617878071cc0
Guilherme Lima <lagartoorei@gmail.com>
2025-10-19 22:11:45 -0300

    pagina frases motivacionais aleatorio

--- script/script.js
+++ script/script.js
@@ -31,3 +31,26 @@
 
             
         }
+
+
+     function gerarFrasesMotivacionais(){
+           const frases = [
+            "Não limite os seus desafios. Desafie os seus limites.",
+            "Viver cada dia como se fosse o último é uma ótima oportunidade para aprender a amar cada segundo do seu dia.",
+            "Se o plano não funcionar, mude o plano, não a meta.",
+            "Você é mais valente do que acredita, mais forte do que parece e está mais preparado do que imagina.",
+            "Que os seus sonhos te levem para onde o seu coração seja feliz.",
+            "Para ter sucesso, você deve eliminar dúvidas, aceitar desafios e rejeitar qualquer negatividade externa",
+            "O que você procura está procurando você.",
+            "Uma meta é um sonho com um prazo.",
+            "Tente mover o mundo - o primeiro passo será mover a si mesmo.",
+            "Disciplina é a ponte entre metas e realizações."
+        ]
+   
+
+        const numeroAleatorio = Math.floor(Math.random()*10)
+        const frase = frases[numeroAleatorio]
+
+        document.getElementById('frase').innerHTML = frase
+    }
+
